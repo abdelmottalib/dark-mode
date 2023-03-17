3c3ead5af1deecc55faa1e7e45e6a34d3fc2c7ba
aqiouami <abdelmottaliblol@gmail.cim>
2023-03-17 14:52:53 +0100

    dark mode

---
+++ script.js
@@ -0,0 +1,9 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+
+function switchTheme(event) {
+	console.log(event);
+}
+
+
+toggleSwitch.addEventListener('change', switchTheme);
